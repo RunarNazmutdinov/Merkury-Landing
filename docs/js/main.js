06817a7a8f9838dc82d6096f0f5e781e7751db58
Runar <github.runar@gmail.com>
2019-10-20 23:05:16 +0300

    Editing in index.html

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,41 +3,21 @@
 	  var pull = $("#navigation-toogle");
 	  var nav = $(".navigation ul");
 
+	   // Описываем событие при нажатии на кнопку
+	   $(pull).on("click", function(e) {
 
+	   // Отменяем стандартное поведение ссылки в браузере
+	   e.preventDefault();
 
-
-	  // Описываем событие при нажатии на кнопку
-	  $(pull).on("click", function(e) {
-
-	    // Отменяем стандартное поведение ссылки в браузере
-	    e.preventDefault();
-
-
-	    // Открываем / Скрываем меню
-	    $(nav).slideToggle();
+	   // Открываем / Скрываем меню
+	   $(nav).slideToggle();
 
 	  });
 
-	  // При изменении размера окна,в большую сторону,если меню было скрыто,показывать его.
 
 	  //slide2id - плавная прокрутка по ссылкам внутри страницы
 	  $("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
 	    highlightSelector: "nav a"
-	  });
-
-	});
-
-
-	// Запуск Owl Carousel
-
-	$(document).ready(function() {
-
-	  var owl = $("#owl-demo");
-
-	  owl.owlCarousel({
-	    navigation: true,
-	    singleItem: true,
-	    transitionStyle: "backSlide"
 	  });
 
 	});
