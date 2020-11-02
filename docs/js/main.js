cc3b18ed34c69c73e197e001e43ff3ac98b13f60
Runar <github.runar@gmail.com>
2020-11-02 20:21:03 +0300

    commit1

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,37 +1,30 @@
-	$(document).ready(function() {
-	    // Cоздаем переменные для кнопки и для меню
-	    var pull = $("#navigation-toggle");
-	    var nav = $(".navigation ul");
+$(document).ready(function () {
+  // Cоздаем переменные для кнопки и для меню
+  var pull = $("#navigation-toggle");
+  var nav = $(".navigation ul");
 
-	    // Описываем событие при нажатии на кнопку
-	    $(pull).on("click", function(e) {
+  // Описываем событие при нажатии на кнопку
+  $(pull).on("click", function (e) {
+    // Отменяем стандартное поведение ссылки в браузере
+    e.preventDefault();
 
-	        // Отменяем стандартное поведение ссылки в браузере
-	        e.preventDefault();
+    // Открываем / Скрываем меню
+    $(nav).slideToggle();
+  });
 
-	        // Открываем / Скрываем меню
-	        $(nav).slideToggle();
+  // Скрываем меню при нажатии на ссылку
 
-	    });
+  var w = $(window).width();
+  if (w < 992) {
+    $("nav.navigation a").on("click", function () {
+      $(nav).slideToggle();
+    });
+  }
 
-
-	    // Скрываем меню при нажатии на ссылку
-
-	    var w = $(window).width();
-	    if (w < 992) {
-	        $('nav.navigation a').on("click", function() {
-	            $(nav).slideToggle();
-
-	        });
-	    }
-
-
-
-
-
-	    //slide2id - плавная прокрутка по ссылкам внутри страницы
-	    $("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
-	        highlightSelector: "nav a"
-	    });
-
-	});
+  //slide2id - плавная прокрутка по ссылкам внутри страницы
+  $("nav a").mPageScroll2id({
+    highlightSelector: "nav a",
+  });
+});
+//a[href='#top']
+//,a.PageScroll2id
