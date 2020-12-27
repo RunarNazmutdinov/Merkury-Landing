6079d9014c9439f9d1f42ea48191298c5aef04a7
Runar <github.runar@gmail.com>
2020-12-27 17:35:34 +0300

    fixed bugs

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,30 +1,37 @@
 $(document).ready(function () {
-  // Cоздаем переменные для кнопки и для меню
-  var pull = $("#navigation-toggle");
-  var nav = $(".navigation ul");
-
-  // Описываем событие при нажатии на кнопку
-  $(pull).on("click", function (e) {
-    // Отменяем стандартное поведение ссылки в браузере
-    e.preventDefault();
-
-    // Открываем / Скрываем меню
-    $(nav).slideToggle();
-  });
-
-  // Скрываем меню при нажатии на ссылку
-
-  var w = $(window).width();
-  if (w < 992) {
-    $("nav.navigation a").on("click", function () {
-      $(nav).slideToggle();
-    });
-  }
-
   //slide2id - плавная прокрутка по ссылкам внутри страницы
   $("nav a").mPageScroll2id({
     highlightSelector: "nav a",
   });
+
+  // Мобильная навигация
+  var navToggleButton = $("#navigation__button");
+  var navBlockOpen = "navigation__list--open";
+  var navToggleIcon = $(".navigation__toggle .fa");
+  var navBlock = $(".navigation__list");
+  var navLink = $(".navigation__list a");
+
+  // События по клику на иконку
+  navToggleButton.on("click", function (e) {
+    e.preventDefault();
+    navBlock.toggleClass(navBlockOpen);
+    navButtonToggle();
+  });
+
+  // События по клику на ссылки
+  navLink.on("click", function () {
+    if (navBlock.hasClass(navBlockOpen)) {
+      navButtonToggle();
+    }
+    navBlock.removeClass(navBlockOpen);
+  });
+
+  // Функция для анимации иконки
+  function navButtonToggle() {
+    if (navToggleButton.hasClass("active")) {
+      navToggleButton.removeClass("active");
+    } else {
+      navToggleButton.addClass("active");
+    }
+  }
 });
-//a[href='#top']
-//,a.PageScroll2id
